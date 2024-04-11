9e86fc0f43d3071b92a36935d842d2f81a0ae41b
DoDevil46 <163332693+DoDevil46@users.noreply.github.com>
2024-04-12 06:23:48 +0700

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.aidiangames.merge.gp"
+#define GamePackageName "com.YEMA.FinalSquad"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
